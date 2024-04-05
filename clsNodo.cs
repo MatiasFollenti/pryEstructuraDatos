438af713e31a0c745826e3dada952c23a1e73f41
root <mgiraudo@ies21.edu.ar>
2024-04-05 14:21:36 -0300

    2024/04/05 creacion de clase Nodo

---
+++ clsNodo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryEstructuraDatos
+{
+    internal class clsNodo
+    {
+        //campos del nodo
+        private Int32 cod;
+        private String nom;
+        private String tram;
+        private clsNodo sig;
+        //propiedades -> como un campo pero que permite codigo dentro
+        //get para la lectura
+        //set para la escritura
+        public Int32 Cod { get { return cod; } set { cod = value; } }
+        public String Nom { get { return nom; } set { nom = value; } }
+        public String Tram { get { return tram; } set { tram = value; } }
+        public clsNodo Sig { get { return sig; } set { sig = value; } }
+
+    }
+}
